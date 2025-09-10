f57c52065fef2248e6c54216544deeaf6d9537fa
Baguette365 <baguette.365@gmail.com>
2025-09-10 15:38:59 +0200

    adding and editing files

--- main.lua
+++ main.lua
@@ -1,6 +1,17 @@
 --[[
     *** IMPORTANT ***
 
-please read README;md
+please read README.md
 
---]
+--]]
+
+local tick = require 'tick'
+
+
+function love.load()
+    tick.framerate = 240 -- Limit framerate to 60 frames per second.
+end
+
+function love.update(dt)
+
+end
