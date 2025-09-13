280be6bf3a5b42445691182f267af556bc0a78c1
Baguette365 <baguette.365@gmail.com>
2025-09-13 18:23:56 +0200

    adding and editing files

--- main.lua
+++ main.lua
@@ -6,19 +6,19 @@ please read README.md
 --]]
 
 function imports()
-    tick = require "src.packages.tick"
-    json = require "src.packages.json"
-    data = require "data"
+    Tick = require "src.packages.tick"
+    Json = require "src.packages.json"
+    Data = require "data"
 end
 
 
 function love.load()
     imports()
-    tick.framerate = 240 -- Limit framerate to 240 frames per second.
+    Tick.framerate = 240 -- Limit framerate to 240 frames per second.
 end
 
 function love.update(dt)
-    if data.window and data.window.title then
+    if Data.window and Data.window.title then
         love.window.setTitle(data.window.title)
     else
         love.window.setTitle(tostring(dt))
---
+++ src/nodes/brick.lua
@@ -0,0 +1,58 @@
+--[[
+
+The basic brick that while make after all of the futur bricks of Bricky
+
+--]]
+
+Brick = {}
+Brick.__index = Brick
+function checkType(value, expected, name)
+    assert(type(value) == expected, name .. " must be " .. expected)
+end
+
+function greet(name, age)
+    checkType(name, "string", "name")
+    checkType(age, "number", "age")
+    print("Bonjour " .. name .. ", tu as " .. age .. " ans.")
+end
+
+
+function Brick:new(position, rotation, scale)
+    local self = setmetatable({}, Brick)
+    self.position = position
+    self.rotation = rotation
+    self.scale = scale
+    return self
+end
+
+function Brick:move(NewPosition, IsRelative)
+    if not IsRelative then
+        self.position = NewPosition
+    else
+        self.position = self.position + NewPosition
+    end
+end
+
+function Brick:rotate(NewRotation, IsRelative)
+    if not IsRelative then
+        self.rotation = NewRotation
+    else
+        self.rotation = self.rotation + NewRotation
+    end
+end
+
+function Brick:scale(NewScale, IsAbsolute)
+    if not IsAbsolute then
+        self.scale = self.scale * NewScale
+    else
+        self.scale = NewScale
+    end
+end
+
+
+
+
+
+
+
+
