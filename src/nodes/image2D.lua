f4bc88f73e15ee7a239f068451ed1e52bf917d08
Baguette365 <baguette.365@gmail.com>
2025-09-14 12:12:48 +0200

    adding and editing files

--- src/nodes/image2D.lua
+++ src/nodes/image2D.lua
@@ -4,3 +4,20 @@ An basic image brick that while display a sprite in 2D
 
 --]]
 
+require "brick"
+
+-- Classe "Enfant" qui hérite de Personne
+Image2D = setmetatable({}, {__index = })
+Image2D.__index = Image2D
+
+function Enfant:new(nom, age, jouet)
+    local self = setmetatable(Personne:new(nom, age), Enfant)
+    self.jouet = jouet
+    return self
+end
+
+function Enfant:sePresenter()
+    -- on peut réutiliser la méthode parent
+    Personne.sePresenter(self)
+    print("Et je joue avec " .. self.jouet)
+end
